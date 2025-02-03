68d30eb992cab9c18a1c8708cf49a14bf728978f
hemanthh2005 <hemanthkumar34174@gmail.com>
2025-02-04 02:08:10 +0530

    Delete pr2 directory

--- pr2/e2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sb
-
-sb.set(style='whitegrid')
-dataset=pd.read_csv('dataset2.csv')
-dataset.info()
-sb.relplot(x='Maths Score',y='Science Score',hue='Gender',style='Parents Degree',data=dataset)
-plt.show()
